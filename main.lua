9a43d807dfa16c127e1a64b48f3aabc5e27027cc
Marbletm <noahtekent@gmail.com>
2024-06-01 00:32:02 +0200

    Code refactoring

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- Instantiate variables
 local query = get("query");
 local btn = get("search-button")
 local rndmbtn = get("random-button")
@@ -7,10 +8,7 @@ local itemName = get("item-name", true);
 local itemUrl = get("item-url", true);
 local itemIp = get("item-ip", true);
 
-for index, item in pairs(items) do
-    item.set_opacity(0);
-end
-
+-- Fetch data
 local response = fetch({
     url = "https://api.buss.lol/domains",
     method = "GET",
@@ -18,55 +16,27 @@ local response = fetch({
     body = ""
 });
 
-function queryServer(content)
+-- Main thread
+function main()
+    clearItems();
+end
+
+-- Declare functions
+------------------------------------
+-- Clears all items from the results.
+function clearItems()
     for index, item in pairs(items) do
         item.set_opacity(0);
-    end
-
-    local filtered = {};
-    for index, item in pairs(response) do
-        local url = (item["name"] .. "." .. item["tld"]);
-        if string.find(string.lower(url), string.lower(content)) then
-            table.insert(filtered, item);
-        end
-    end
-
-    for index, item in pairs(filtered) do
-        local itemEl = items[index];
-        local nameEl = itemName[index];
-        local ipEl = itemIp[index];
-        local urlEl = itemUrl[index];
-
-        local url = "buss://" .. item["name"] .. "." .. item["tld"];
-
-        itemEl.set_opacity(1);
-        nameEl.set_content(item["name"]);
-        ipEl.set_content(item["ip"]);
-        urlEl.set_content(url);
-        urlEl.set_href(url);
     end
 end
 
-query.on_submit(queryServer);
+-- Displays the given item at the given index in the results.
+function displayItem(index, item)
+    local itemEl = items[index];
+    local nameEl = itemName[index];
+    local ipEl = itemIp[index];
+    local urlEl = itemUrl[index];
 
-btn.on_click(function()
-    queryServer(query.get_content())
-end)
-
-rndmbtn.on_click(function()
-    for index, item in pairs(items) do
-        item.set_opacity(0);
-    end
-
-    local field = items[1];
-    local itemEl = items[1];
-    local nameEl = itemName[1];
-    local ipEl = itemIp[1];
-    local urlEl = itemUrl[1];
-
-    field.set_opacity(1);
-
-    local item = response[math.random(#response)];
     local url = "buss://" .. item["name"] .. "." .. item["tld"];
 
     itemEl.set_opacity(1);
@@ -74,4 +44,45 @@ rndmbtn.on_click(function()
     ipEl.set_content(item["ip"]);
     urlEl.set_content(url);
     urlEl.set_href(url);
+end
+
+-- Displays an array of items in the results.
+function displayItems(arr)
+    clearItems();
+    for index, item in pairs(arr) do
+        displayItem(index, item)
+    end
+end
+
+-- Filters all the items for items that match the given query string.
+function filterItems(queryString)
+    local filtered = {};
+    for index, item in pairs(response) do
+        local url = (item["name"] .. "." .. item["tld"]);
+        if string.find(string.lower(url), string.lower(queryString)) then
+            table.insert(filtered, item);
+        end
+    end
+    return filtered;
+end
+
+-- Returns a random item from the list of items.
+function getRandomItem()
+    return response[math.random(#response)];
+end
+
+-- Retrieve the contents of the input and apply the query.
+function applyQuery(queryString)
+    displayItems(filterItems(query.get_content()));
+end
+
+-- Event Listeners
+query.on_submit(applyQuery);
+btn.on_click(applyQuery);
+rndmbtn.on_click(function()
+    clearItems();
+    displayItem(1, getRandomItem());
 end)
+
+-- Run main thread
+main();
